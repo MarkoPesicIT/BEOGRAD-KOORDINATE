73c911be1f8164eb5297a0c8af35495b49c17dab
KrticaTheMadLad <100162583+KrticaTheMadLad@users.noreply.github.com>
2023-05-10 23:30:07 +0200

    #

--- klasicizam.js
+++ klasicizam.js
@@ -5,15 +5,15 @@ const nextButton = document.querySelector('.slider-next');
 const slides = [
       {
         image: 'Materijal/Kolazi/12. Kolaž - Klasicizam .jpg',
-        text: 'Klasicizam je stil u umetnosti sa kraja 18. i iz prve polovine 19. veka, koji je ponovo afirmisao osobenosti arhitekture antičkih uzora, oslanjajući se na racionalističku filozofiju i nove društvene okolnosti posle francuske buržoaske revolucije. Bazira se na estetskim teorijama Vinkelmana, po kojima je lepota stvorena već u antičkoj umetnosti, sa osnovnim odlikama odmerenosti, simetrije, reda i jasnoće. Najbolje se može okarakterisati njegovom maksimom „plemenite jednostavnosti i tihe veličine“.'
+        text: 'Класицизам је стил у уметности са краја 18. и из прве половине 19. века, који је поново афирмисао особености архитектуре античких узора, ослањајући се на рационалистичку филозофију и нове друштвене околности после француске буржоаске револуције. Базира се на естетским теоријама Винкелмана, по којима је лепота створена већ у античкој уметности, са основним одликама одмерености, симетрије, реда и јасноће. Најбоље се може окарактерисати његовом максимом „племените једноставности и тихе величине“.'
       },
       {
         image: 'Materijal/Kolazi/12. Kolaž - Klasicizam .jpg',
-        text: 'To je stil sa kojim se arhitektura Srbije srela posle dobijanja određenih sloboda unutar Osmanske carevine i prvi je iskorak van granica tradicionalne balkanske gradnje. Malo zastupljen na samom početku, pre svega zbog ograničavajućih ekonomskih i političkih faktora, klasicizam u Srbiji dobija puni zamah posle nešto više od pola veka, ovog puta kroz forme obnovljenog klasicizma, aktuelnog između dva svetska rata. Štaviše, objekti podignuti u ovom periodu dali su pečat centru Beograda i kao takvi postali su jedan od kamena međaša srpske arhitekture.'
+        text: 'То је стил са којим се архитектура Србије срела после добијања одређених слобода унутар Османске царевине и први је искорак ван граница традиционалне балканске градње. Мало заступљен на самом почетку, пре свега због ограничавајућих економских и политичких фактора, класицизам у Србији добија пуни замах после нешто више од пола века, овог пута кроз форме обновљеног класицизма, актуелног између два светска рата. Штавише, објекти подигнути у овом периоду дали су печат центру Београда и као такви постали су један од камена међаша српске архитектуре.'
       },
       {
         image: 'Materijal/Kolazi/12. Kolaž - Klasicizam .jpg',
-        text: 'Stvorena je kao arhitektura bazirana na moralnom kriterijumu, u vreme kada su se kreirale nove moderne države, sa svim postulatima trodelne vlasti (zakonodavne, izvršne i sudske), kada se upravljačka moć prenosila sa monarha na narod, kada je bilo potrebno da se konstituiše demokratski sistem kao izraz volje građana. Sa svim atributima prosvetiteljstva, ovakva arhitektura nosi u sebi večite ideje pravednosti i večitu estetiku formiranu tokom dve hiljade godina.'
+        text: 'Створена је као архитектура базирана на моралном критеријуму, у време када су се креирале нове модерне државе, са свим постулатима троделне власти (законодавне, извршне и судске), када се управљачка моћ преносила са монарха на народ, када је било потребно да се конституише демократски систем као израз воље грађана. Са свим атрибутима просветитељства, оваква архитектура носи у себи вечите идеје праведности и вечиту естетику формирану током две хиљаде година.'
       }
     ];
     
