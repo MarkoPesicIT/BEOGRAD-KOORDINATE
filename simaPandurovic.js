83e1afbafc592888ea8209b91e1512c3b7fa2834
KrticaTheMadLad <100162583+KrticaTheMadLad@users.noreply.github.com>
2023-05-11 18:14:12 +0200

    Пребацено све на ћирилицу

--- simaPandurovic.js
+++ simaPandurovic.js
@@ -5,11 +5,11 @@ const nextButton = document.querySelector('.slider-next');
 const slides = [
       {
         image: 'Materijal/Licnosti/sPandurovic.jpg',
-        text: 'Sima Pandurović (1893 – 1960) poznati je srpski pesnik. Službeničku karijeru otpočeo je kao profesor u valjevskoj gimnaziji, a u Srpskoj kraljevsjkoj četvrtoj gimnaziji radio je od 1910. do 1913. godine.Upravo u to vreme doživeo je najveću slavu kao mladi i talentovani pesnik (sa zbirkom Posmrtne počasti), ali i ubitačnu kritiku Jovana Skerlića, koji ga je zajedno sa Disom oglasio za rodonačelnika „bolesnog pesimizma“ u srpskoj poeziji. Za vreme Prvog svetskog rata Austrijanci su ga internirali u logore u Boldogasonju i Nežideru. '
+        text: 'Сима Пандуровић (1893 – 1960) познати је српски песник. Службеничку каријеру отпочео је као професор у ваљевској гимназији, а у Српској краљевсјкој четвртој гимназији радио је од 1910. до 1913. године.Управо у то време доживео је највећу славу као млади и талентовани песник (са збирком Посмртне почасти), али и убитачну критику Јована Скерлића, који га је заједно са Дисом огласио за родоначелника „болесног песимизма“ у српској поезији. За време Првог светског рата Аустријанци су га интернирали у логоре у Болдогасоњу и Нежидеру.'
       },
       {
         image: 'Materijal/Licnosti/sPandurovic.jpg',
-        text: 'Radni vek je proveo kao sekretar Ministarstva prosvete i pomoćnik upravnika Narodne biblioteke. Uređivao je izuzetno uticajan književni časopis „Misao“ i prevodio Molijera i Šekspira.Za vreme nemačke okupacije (1941 – 1944) bio je jedno vreme šef odeljenja Beogradske opštine. Zbog toga, kao i zbog nekih predavanja tokom okupacije, komunisti su ga poslali tzv. Sud časti, i on je, posle osude, do smrti tavorio u siromaštvu.* Gimnaziju „Sveti Sava“ u Beogradu prvobitno se zvala Srpska kraljevsjka četvrta gimnazija od njenog osnivanja 1910. godine do 1919. godine.'
+        text: 'Радни век је провео као секретар Министарства просвете и помоћник управника Народне библиотеке. Уређивао је изузетно утицајан књижевни часопис „Мисао“ и преводио Молијера и Шекспира.За време немачке окупације (1941 – 1944) био је једно време шеф одељења Београдске општине. Због тога, као и због неких предавања током окупације, комунисти су га послали тзв. Суд части, и он је, после осуде, до смрти таворио у сиромаштву.* Гимназију „Свети Сава“ у Београду првобитно се звала Српска краљевсјка четврта гимназија од њеног оснивања 1910. године до 1919. године.'
       }
     ];
     
--- skc.js
+++ skc.js
@@ -5,23 +5,23 @@ const nextButton = document.querySelector('.slider-next');
 const slides = [
       {
             image: 'Materijal/Kolazi/14. Kolaž - Zgrada Studentskog kulturnog centra (SKC).jpg',
-            text: 'Podignut kao Zgrada oficirskog doma u Beogradu, današnji SKC natopljen je događajima koji su obeležili istoriju glavnog grada, ali i države. Nekada dom kamerne muzike, balova i vojničkih zabava, potom mesto gde su planirani prevrat i studentski bunt. SKC danas iz dremeža bude rok koncerti sećajući ga na dane kada su se, iza prelepe fasade, poveravale najveće tajne. Zgrada oficirskog doma nastala je iz ideje Aleksandra I Obrenovića da oficirima srpske vojske podari mesto gde će se okupljati, družiti i zabavljati na balovima i izložbama.'
+            text: 'Подигнут као Зграда официрског дома у Београду, данашњи СКЦ натопљен је догађајима који су обележили историју главног града, али и државе. Некада дом камерне музике, балова и војничких забава, потом место где су планирани преврат и студентски бунт. СКЦ данас из дремежа буде рок концерти сећајући га на дане када су се, иза прелепе фасаде, поверавале највеће тајне. Зграда официрског дома настала је из идеје Александра И Обреновића да официрима српске војске подари место где ће се окупљати, дружити и забављати на баловима и изложбама.'
       },
       {
             image: 'Materijal/Kolazi/14. Kolaž - Zgrada Studentskog kulturnog centra (SKC).jpg',
-            text: 'Zgrada SKC danas je zaštićeni spomenik kulture, u kome se održavaju izložbe, koncerti, pozorišni programi... i to u pet galerija – V.I.P. Art Galerija, Cirkus galerija, Art@Art galerija, Galerija SKC i Srećna galerija. Takođe, tu su i redakcije za muzički i pozorišni program. U lokalima u prizemlju objekta, smeštene su knjižare i kafić, a kultna manifestacija, od 2003. godine postao je Međunarodni salon stripa.'
+            text: 'Зграда СКЦ данас је заштићени споменик културе, у коме се одржавају изложбе, концерти, позоришни програми... и то у пет галерија – В.И.П. Арт Галерија, Циркус галерија, Арт@Арт галерија, Галерија СКЦ и Срећна галерија. Такође, ту су и редакције за музички и позоришни програм. У локалима у приземљу објекта, смештене су књижаре и кафић, а култна манифестација, од 2003. године постао је Међународни салон стрипа.'
       },
       {
             image: 'Materijal/Kolazi/14. Kolaž - Zgrada Studentskog kulturnog centra (SKC).jpg',
-            text: 'Kompletna zamisao definitivan izgled dobija 3. aprila 1971. kada je zvanično osnovan Studentski kulturni centar. Naredne godine učinile su da SKC postane jedno od glavnih okupljališta urbane omladine. Ovde se razvijala avangarda, rađali se autori i obrazovala omladina.'
+            text: 'Комплетна замисао дефинитиван изглед добија 3. априла 1971. када је званично основан Студентски културни центар. Наредне године учиниле су да СКЦ постане једно од главних окупљалишта урбане омладине. Овде се развијала авангарда, рађали се аутори и образовала омладина.'
       },
       {
             image: 'Materijal/Kolazi/14. Kolaž - Zgrada Studentskog kulturnog centra (SKC).jpg',
-            text: 'U vremenu nakon Drugog svetskog rata, "dvorac u centru" postaje Dom tajne policije. Tu su prikazivane projekcije filmova sa Zapada i pravljene prve rok igranke. Ideja vlasti je bila imati mesto gde će studentske revolucionarne ideje i "protiv državno delovanje" biti pod jednim krovom i lake za kontrolu i nadzor. Da je takvih ideja bilo, jasno su pokazali studentski protesti 1968. godine, posle kojih je Oficirski dom predat na upravu Beogradskom univerzitetu. '
+            text: 'У времену након Другог светског рата, "дворац у центру" постаје Дом тајне полиције. Ту су приказиване пројекције филмова са Запада и прављене прве рок игранке. Идеја власти је била имати место где ће студентске револуционарне идеје и "против државно деловање" бити под једним кровом и лаке за контролу и наџор. Да је таквих идеја било, јасно су показали студентски протести 1968. године, после којих је Официрски дом предат на управу Београдском универзитету.'
       },
       {
             image: 'Materijal/Kolazi/14. Kolaž - Zgrada Studentskog kulturnog centra (SKC).jpg',
-            text: '1895. godine, a prema projektima dvojice arhitekta Jovana Ilkića i Milorada Ruvidića, podignut je Oficirski dom. S obzirom na "vojnu" namenu, odlučeno je da zgrada ima oblik zamka i, u to vreme, bila je zaista jedinstvena beogradska građevina. Svečano otvaranje Oficirskog doma odigralo se 14. avgusta 1895. na rođendan kralja Aleksandra. U svom govoru, kralj je rekao kako je Oficirski dom "dokaz moje brige za usavršavanjem srpske odbrambene snage. Preporučujem da se u njemu razvijaju vojničke vrline i druželjublje".'
+            text: '1895. године, а према пројектима двојице архитекта Јована Илкића и Милорада Рувидића, подигнут је Официрски дом. С обзиром на "војну" намену, одлучено је да зграда има облик замка и, у то време, била је заиста јединствена београдска грађевина. Свечано отварање Официрског дома одиграло се 14. августа 1895. на рођендан краља Александра. У свом говору, краљ је рекао како је Официрски дом "доказ моје бриге за усавршавањем српске одбрамбене снаге. Препоручујем да се у њему развијају војничке врлине и дружељубље".'
       }
 ];
     
